a860ac6305fe3569f97259b0bc10964f01ddbf18
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-10-29 17:53:01 +0100

    add the graph ql servere and start to play with some api responses from the native app

--- client/App.tsx
+++ client/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Gareth rocks!</Text>
+      <View style={styles.button}>
+        <Button
+          title="click to get data from api"
+          onPress={() => {
+            fetch('http://localhost:4000/graphql', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+              },
+              body: JSON.stringify({ query: '{ hello }' })
+            })
+              .then(r => r.json())
+              .then(data => console.log('data returned:', data));
+          }}
+        >
+          Click me!
+        </Button>
+      </View>
     </View>
   );
 }
@@ -14,6 +33,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  button: {
+    flexDirection: 'row'
+  }
 });
