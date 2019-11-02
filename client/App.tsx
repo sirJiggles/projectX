1cbe8c465d1f55af87b3260513c8b6de97f1850f
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-11-02 22:34:06 +0100

    got some basic tabs going on in the app

--- client/App.tsx
+++ client/App.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import Application from './components/app/Application';
+import Home from './components/home/Home';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
 });
 
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Home,
+  Settings: SettingsScreen
+});
+
+const NavigationAppContainer = createAppContainer(TabNavigator);
+
 export default function App() {
   return (
     <ApolloProvider client={client}>
-      <Application />
+      <NavigationAppContainer />
     </ApolloProvider>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
@@ -25,6 +26,7 @@ var root = {
 };
 
 var app = express();
+app.use(cors());
 app.use(
   '/graphql',
   graphqlHTTP({
