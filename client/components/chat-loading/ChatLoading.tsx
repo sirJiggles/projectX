4faf439c1de555daace205bc09c2f3806668dceb
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-11-03 00:07:08 +0100

    add loti and a real simple sample animation

--- client/components/chat-loading/ChatLoading.tsx
+++ client/components/chat-loading/ChatLoading.tsx
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, Text, StyleSheet } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 export default class ChatLoading extends Component {
   render() {
     return (
-      <View>
-        <Button
-          title="Go to chat screen"
-          onPress={() => {
-            this.props.navigation.navigate('Chat');
-          }}
-        >
-          Go to chat
-        </Button>
+      <View style={style.container}>
+        <View style={style.button}>
+          <Button
+            title="Go to chat screen"
+            onPress={() => {
+              this.props.navigation.navigate('Chat');
+            }}
+          >
+            Go to chat
+          </Button>
+        </View>
+        <View style={style.loading}>
+          <LottieView source={require('./animation.json')} autoPlay loop />
+        </View>
       </View>
     );
   }
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignContent: 'center'
+  },
+  button: {
+    marginBottom: 30,
+    flex: 1
+  },
+  loading: {
+    flex: 2
+  }
+});
