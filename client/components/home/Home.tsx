ba896f4c1e19c2c72f0ff53e6e7e5ed2a3ff5120
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-11-03 22:04:42 +0100

    move the message sender to a new component

--- client/components/home/Home.tsx
+++ client/components/home/Home.tsx
@@ -1,44 +1,14 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Quote from '../quote/Quote';
-import colors from '../../ui/colors';
+import MessageSender from '../message-sender/MessageSender';
 
 export default class Home extends Component {
-  state: {
-    messageToSend: '';
-  };
-
-  sendMessage() {
-    console.log('we should now send the message!');
-    console.log(this.state.messageToSend);
-    // clear the text input after we clear the text
-    this.setState({});
-    // @TODO send the message to the server!
-  }
-
   render() {
     return (
       <View style={styles.container}>
         <Quote />
-        <View style={styles.messageSender}>
-          <Text style={styles.messageInstructions}>
-            Send a message to chat:
-          </Text>
-          <TextInput
-            style={styles.messageInput}
-            onChangeText={text => {
-              this.setState({ ...this.state, messageToSend: text });
-            }}
-          />
-          <Button
-            title="send a message"
-            onPress={() => {
-              this.sendMessage();
-            }}
-          >
-            Send message
-          </Button>
-        </View>
+        <MessageSender />
       </View>
     );
   }
@@ -50,23 +20,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
-  },
-  messageSender: {
-    marginTop: 40,
-    display: 'flex',
-    flexDirection: 'column',
-    alignContent: 'flex-start',
-    width: '70%'
-  },
-  messageInstructions: {
-    color: colors.lightContext.hex
-  },
-  messageInput: {
-    height: 50,
-    borderColor: colors.lightContext.hex,
-    borderWidth: 2,
-    borderRadius: 3,
-    paddingLeft: 20,
-    paddingRight: 20
   }
 });
---
+++ client/components/message-sender/MessageSender.tsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import colors from '../../ui/colors';
+
+export default class MessageSender extends Component {
+  state: {
+    messageToSend: '';
+  };
+
+  sendMessage() {
+    console.log('we should now send the message!');
+    console.log(this.state.messageToSend);
+    // clear the text input after we clear the text
+    this.setState({});
+    // @TODO send the message to the server!
+  }
+
+  render() {
+    return (
+      <View style={styles.messageSender}>
+        <Text style={styles.messageInstructions}>Send a message to chat:</Text>
+        <TextInput
+          style={styles.messageInput}
+          onChangeText={text => {
+            this.setState({ ...this.state, messageToSend: text });
+          }}
+        />
+        <Button
+          title="send a message"
+          onPress={() => {
+            this.sendMessage();
+          }}
+        >
+          Send message
+        </Button>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  messageSender: {
+    marginTop: 40,
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'flex-start',
+    width: '70%'
+  },
+  messageInstructions: {
+    color: colors.lightContext.hex
+  },
+  messageInput: {
+    height: 50,
+    borderColor: colors.lightContext.hex,
+    borderWidth: 2,
+    borderRadius: 3,
+    paddingLeft: 20,
+    paddingRight: 20
+  }
+});
