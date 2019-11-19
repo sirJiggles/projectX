48264c9533c116177ff904f69fe0ea8e96700f93
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-11-19 22:10:34 +0100

    we need to think about how we are going to do the users before moving on

--- client/graph/mutations/createChat.ts
+++ client/graph/mutations/createChat.ts
@@ -1,8 +1,8 @@
 import { gql } from 'apollo-boost';
 
 export default gql`
-  mutation createChat($name: String!, $members: String) {
-    createChat(name: $name, members: []) {
+  mutation createChat($name: String!, $members: [User!]!) {
+    createChat(name: $name, members: $members) {
       name
       members
     }
